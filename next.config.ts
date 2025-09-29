bc4dea9417eb865c1fdc888e15aae486f734e595
JIRVII <jorry.328@gmail.com>
2025-09-29 20:59:42 +0300

    Configure next.config.js for leaflet/react-leaflet externals

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... أي إعدادات أخرى لديك
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, 'react-leaflet', 'leaflet'];
+    }
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
